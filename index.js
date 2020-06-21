3b09e4bd5c04ee3b6152565fe054225f6c498abd
Hussein Talal <hossientalal2@gmail.com>
2020-06-21 09:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ class Circle{
     return Math.PI * this.radius ** 2
   }
   set diameter(diameter){
-
+    this.radius = diameter/2;
+  }
+  set circumference(circumference){
+    this.radius = circumference/(2 * Math.PI);
+  }
+  set area(area){
+    this.radius = Math.sqrt(area/Math.PI);
   }
 }
